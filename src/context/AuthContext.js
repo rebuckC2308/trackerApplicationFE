bfa609093c3d86ce90249c1c596a29d1cee95709
Chris Rebuck <rebuckc2308@gmail.com>
2022-01-08 12:44:05 -0600

    Created a reusable NavLink component between our SignIn and SignUp screens.  Also cleared the errorMessage when navigating between screens.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -8,34 +8,46 @@ const authReducer = (state, action) => {
   switch (action.type) {
       case 'add_error':
           return {...state, errorMessage: action.payload}
-      case 'signup':
-          return { errorMessage: '', token: action.payload}
+      case 'signin':
+          return { errorMessage: '', token: action.payload }
+      case 'clear_error_message':
+          return { ...state, errorMessage: '' }      
     default:
       return state;
   }
 };
+
+const clearErrorMessage = (dispatch) => () => {
+    dispatch({ type: 'clear_error_message' })
+}
 
 const signup = (dispatch) => async ({ email, password }) => {
     try{
       const response = await trackerApi.post('/signup', { email, password } )
       await AsyncStorage.setItem('token', response.data.token);
 
-      dispatch({type: 'signup', payload: response.data.token});
+      dispatch({type: 'signin', payload: response.data.token});
 
       //navigate to a flow
       navigate('TrackList');
 
     } catch (error) {
-        dispatch({ type: 'add_error', payload: 'Something went wrong during signup' });
+        dispatch({ type: 'add_error', payload: 'Something went wrong during sign up' });
     };
   };
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    //try to sign in
-    //handle success by updating state
-    //handle failure by showing error message (somehow)
-  };
+const signin = (dispatch) => async({ email, password }) => {
+      try {
+          const response = await trackerApi.post('/signin', { email, password });
+          await AsyncStorage.setItem('token', response.data.token);
+
+          dispatch({type: 'signin', payload: response.data.token})
+
+          navigate('TrackList')
+
+      }catch(err){
+        dispatch({type: 'add_error', payload: 'Something went wrong during sign in'})
+      }
 };
 
 const signout = (dispatch) => {
@@ -46,6 +58,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signup, signout },
+  { signin, signup, signout, clearErrorMessage },
   { token: null, errorMessage: '' }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,29 +1,30 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext'
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
-  const {state, signup } = useContext(AuthContext);
-
-
+  const {state, signup, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents 
+        onWillFocus={clearErrorMessage}
+          
+      />
       <AuthForm
         headerText={"Sign up for Tracker"}
         errorMessage={state.errorMessage}
         submitButtonText={"Sign Up"}
         onSubmit={signup}
       />
-    
-      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-          <Spacer>
-            <Text style={styles.link}>Already have an account? Sign in instead</Text>
-          </Spacer>
-      </TouchableOpacity>
+
+      <NavLink
+        text='Already have an account? Sign in here!'
+        routeName='Signin'
+      />
     </View>
   );
 };
@@ -40,10 +41,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 200,
   },
-  link: {
-    color: 'blue',
-    textAlign: 'center'
-  }
+
 });
 
 export default SignupScreen;
